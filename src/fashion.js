37ee1ffbcd476cacb633505566533bcf5760a909
景天 <jingtian@naitang.com>
2016-01-11 18:07:19 +0800

    update

--- src/fashion.js
+++ src/fashion.js
@@ -1,10 +1,10 @@
-var Vue = require("vue"),
-    openApp = require("./module/openApp"),
+var Vue = require("vue")
+openApp = require("./module/openApp"),
     vueApp = require("./module/vueApp"),
     vueInfo = require("./module/vueInfo");
+var vueResource = require('vue-resource');
 
 vueApp.methods.openApp = openApp;
-
 Vue.component('my-info', vueInfo);
-Vue.use(require("vue-resource"));
+Vue.use(vueResource);
 var vm = new Vue(vueApp);
--- src/module/isMobile.js
+++ src/module/isMobile.js
@@ -1,15 +1,12 @@
-
-module.exports = function(){
+module.exports = function() {
     var userAgentInfo = navigator.userAgent;
     var flag = false,
         wx = false;
-    if (userAgentInfo.indexOf('Android') > -1 || userAgentInfo.indexOf('Linux') > -1) { //安卓手机
+    if (userAgentInfo.indexOf('Android') > -1 || userAgentInfo.indexOf('Linux') > -1) {
         flag = "Android";
-    } else if (userAgentInfo.indexOf('iPhone') > -1) { //苹果手机
+    } else if (userAgentInfo.indexOf('iPhone') > -1) {
         flag = "iPhone";
-    } else if (userAgentInfo.indexOf('Windows Phone') > -1) { //winphone手机
-        flag = true;
-    } else if (userAgentInfo.indexOf('iPad') > -1) { //iPad
+    } else if (userAgentInfo.indexOf('iPad') > -1) {
         flag = "iPhone";
     }
     if (userAgentInfo.indexOf('MicroMessenger') > -1) {
